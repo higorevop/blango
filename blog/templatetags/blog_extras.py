84366bf1f955eafcd7de4d5fee41e415c2d9c6b1
Higor Prado <higorevop@gmail.com>
2023-05-29 20:10:12 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,14 +2,16 @@ from django import template
 from django.contrib.auth import get_user_model
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
+from blog.models import Post
 
 register = template.Library()
 
-@register.filter
-def author_details(author, current_user=None):
-  user_model = get_user_model()
-  if not isinstance(author, user_model):
-      return ""
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+  request = context["request"]
+  current_user = request.user
+  post = context["post"]
+  author = post.author
 
   if author == current_user:
     return format_html("<strong>me</strong>")
@@ -28,3 +30,16 @@ def author_details(author, current_user=None):
       suffix = ""
 
   return format_html('{}{}{}', prefix, name, suffix)
+
+@register.simple_tag
+def row(extras_classes=""):
+  return format_html('<div class="row {}">', extras_classes)
+
+@register.simple_tag
+def endrow():
+  return format_html("</div>")
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+  posts = Post.objects.exclude(pk=post.pk)[:5]
+  return {"title": "Recent Posts", "posts": posts}
